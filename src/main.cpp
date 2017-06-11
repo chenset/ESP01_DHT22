a57872aed3a87ea8f311e575a4302cadef5afe5b
chenset <4199191@qq.com>
2017-06-11 11:06:11 +0800

    use OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// #include <ArduinoOTA.h>
+#include <ArduinoOTA.h>
 #include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
@@ -71,11 +71,35 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
 
+  // OTA
+  ArduinoOTA.onStart([]() { Serial.println("Start"); });
+  ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
   // Print the IP address
   delay(500);
+  for(int whileCount = 0;whileCount < 10;++whileCount){
+      ArduinoOTA.handle();
+      delay(1000);
+  }
 
   DHTSenserUpdate();
-
   DHTSenserPost();
 }
 
